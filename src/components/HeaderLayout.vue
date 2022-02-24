0adbd6a572c24c632cf04e289b325c799721bb30
nttps <teamerze@gmail.com>
2022-02-24 18:08:19 +0700

    change navbar font color to white

--- src/components/HeaderLayout.vue
+++ src/components/HeaderLayout.vue
@@ -3,7 +3,7 @@ import MenuProfile from './MenuProfile.vue';
 </script>
 
 <template lang="">
-    <header class="flex justify-between items-center px-4  bg-[#2a2548] dark:bg-amber-600">
+    <header class="flex justify-between items-center px-4 text-white bg-[#2a2548] dark:bg-amber-600">
       <!-- <div></div> -->
       <div>WAKIE WORD</div>
       <div class="flex items-center">
